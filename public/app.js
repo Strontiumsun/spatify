ff19b3c944775974217b85b54635d352bbca3e53
Sarah Tam <sarahtam718@gmail.com>
2019-04-15 20:16:35 -0500

    kind of working cards appended!

--- public/app.js
+++ public/app.js
@@ -9,56 +9,71 @@ $(document).ready(function () {
     $(".service-button").on("click", function (event) {
         event.preventDefault();
         var salonType = $(this).attr("id")
-        $.get(`/api/salons/services/${salonType}`, function (data) {
-            // console.log("gfxgfxgfx", data)
+        $.get("/api/salons", function (data) {
             for (var i = 0; i < data.length; i++) {
-                console.log(data[i].services)
-                userData = data[i];
-                console.log("all: ", userData)
-                services = data[i].services;
-                image = `${data[i].image}`
-                name = `${data[i].name}`
-                // console.log(name)
-                // $("#result").append(`
-                //     <div class="col s12 m6 l4">
-                //         <div class="card">
-                //             <div class="card-image">
-                //         <img src='${data[i].image}'>
-                //         <span class="card-title">
-                //             <h5>'${data[i].name}'</h5>
-                //         </span>
-                //         <div class="card-content">
-
-                //             <p>'${data[i].services}'</p>
-                //         </div>
-                //     </div>
-                //     `)
+                services = data[i].services
+                name = data[i].name
+                // console.log(data[i].services)
+                // var serv = data[i].services;
+                // userData.push(serv)
+                // console.log(userData[0])
+                // console.log(salonType)
+                if (services == salonType) {
+                    userData.push(name)
+                    // console.log("match: ", services)
+                    // console.log("names: ", name, userData)
+                    cardAppend(data)
+                }
             }
-            // cardAppend(data)
         });
+        // $.get(`/api/salons/services/${salonType}`, function (data) {
+        //     // console.log("gfxgfxgfx", data)
+        //     for (var i = 0; i < data.length; i++) {
+        //         console.log(data[i].services)
+        //         userData = data[i];
+        //         console.log("all: ", userData)
+        //         services = data[i].services;
+        //         image = `${data[i].image}`
+        //         name = `${data[i].name}`
+        //         // console.log(name)
+        //         // $("#result").append(`
+        //         //     <div class="col s12 m6 l4">
+        //         //         <div class="card">
+        //         //             <div class="card-image">
+        //         //         <img src='${data[i].image}'>
+        //         //         <span class="card-title">
+        //         //             <h5>'${data[i].name}'</h5>
+        //         //         </span>
+        //         //         <div class="card-content">
+
+        //         //             <p>'${data[i].services}'</p>
+        //         //         </div>
+        //         //     </div>
+        //         //     `)
+        //     }
+        //     // cardAppend(data)
+        // });
         // });
 
     });
 
     function cardAppend(data) {
-        console.log("name: ", name)
-        userData.push(name)
+        console.log("names: ", name)
         console.log("results: ", userData)
         $("#result").append(`
         <div class="col s12 m6 l4">
             <div class="card">
                 <div class="card-image">
-            <img src='image'>
+            <img src=${image}>
             <span class="card-title">
-                <h5>'name'</h5>
+                <h5>'${name}'></h5>
             </span>
             <div class="card-content">
-
+            <p>'${services}'</p>
             </div>
             </div>
             `)
     }
-    // <p>'${data[i].services}'</p>
 
 
     $("#formSubmit").on("click", function (event) {
