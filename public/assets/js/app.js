ba90478996b25fae16e9ee6fb0227d6f567bcc74
mc0918 <mcampbell0918@gmail.com>
2019-04-16 11:05:32 -0500

    changed css to make cards and buttons more responsive

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -32,31 +32,44 @@ $(document).ready(function() {
     }
   });
 
-    $(".service-button").on("click", function () {
-        // console.log("clicked " + $(this).attr("data-name"))
-        $("#append-here").empty()
-        var buttonName = $(this).attr("data-name");
-        console.log(buttonName)
+  $(".service-button").on("click", function() {
+    // console.log("clicked " + $(this).attr("data-name"))
+    $("#append-here").empty();
+    var buttonName = $(this).attr("data-name");
+    console.log(buttonName);
 
-        $.ajax({
-            url: `/api/salons/services/${buttonName}`,
-            method: "GET"
-        }).then(function (data) {
-            console.log(data[0].name)
-            for (var i = 0; i < data.length; i++) {
-                $("#append-here").append(`
-                <div class="card">
-                <div class="card-image">
-                <img src='${data[i].image}'>
-                <span class="card-title"><h5>${data[i].name}</h5></span>
-                <div class="card-content">
-                    <p>${data[i].services}</p>
-                </div>
-                </div>
-            </div>`)
-            }
-        })
-    })
+    $.ajax({
+      url: `/api/salons/services/${buttonName}`,
+      method: "GET"
+    }).then(function(data) {
+      console.log(data[0].name);
+      for (var i = 0; i < data.length; i++) {
+        // $("#append-here").append(`
+        //         <div class="card">
+        //         <div class="card-image">
+        //         <img src='${data[i].image}'>
+        //         <span class="card-title"><h5>${data[i].name}</h5></span>
+        //         <div class="card-content">
+        //             <p>${data[i].services}</p>
+        //         </div>
+        //         </div>
+        //     </div>`);
+        var card = $("<div>").attr("class", "card");
+        var cardImage = $("<div>").attr("class", "card-image");
+        var image = $("<img>").attr("src", `${data[i].image}`);
+        var cardTitle = $("<span>").attr("class", "card-title");
+        var title = $("<h5>").html(`${data[i].name}`);
+        var cardContent = $("<div>").attr("class", "card-content");
+        var cardText = $("<p>").html(`${data[i].services}`);
+
+        //cardContent.append(cardText);
+        cardTitle.append(title, cardText);
+        cardImage.append(image);
+        card.append(cardImage, cardTitle);
+        $("#append-here").append(card);
+      }
+    });
+  });
 });
 
 // on click button when...
