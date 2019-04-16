f331dc0f7582b61c633d457d4fe4e13239c82aa4
Sarah Tam <sarahtam718@gmail.com>
2019-04-16 18:05:23 -0500

    nothing

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -100,11 +100,19 @@ module.exports = function (app) {
   });
 
   // this route will let us save user data to the database
-  app.post("/api/reservations", function (req, res) {
-    console.log(req.body);
-    // here we'll create a new object with the data from the front end
-    // this route can't be completed without frontend js
-  });
+  //   app.post("/api/reservations", function (req, res) {
+  //     console.log(req.body);
+  //     db.User.create({
+  //       email: req.body.text,
+  //       name: req.body.complete
+  //     }).then(function (dbTodo) {
+  //       // We have access to the new todo as an argument inside of the callback function
+  //       res.json(dbTodo);
+  //     });
+  //   });
+  //   // here we'll create a new object with the data from the front end
+  //   // this route can't be completed without frontend js
+  // });
 
   app.post("/email", function (req, res) {
     //console.log(req);
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,21 +7,17 @@ var Salon = require("../models/salon");
 router.get("/", function (req, res) {
   res.render(
     "index"
-    //add object here
-    //, { title: "blah" }
   );
 });
 //email script test========================
 //var script = [{ script: "../email.js" }];
 //email script test========================
 
-// router.get("/form", function(req, res) {
-//   res.render(
-//     "form"
-//     //add object here
-//     //, { title: "blah" }
-//   );
-// });
+router.get("/form", function (req, res) {
+  res.render(
+    "form"
+  );
+});
 
 router.get("/services", function (req, res) {
   res.render("services");
